06fed85958a3d259be82602140ad226597b97d5a
Vasilii Sukhorukov <vasilii.matrix@gmail.com>
2025-11-14 01:30:36 +0300

    Update TelegramBotController.php

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -4,25 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
+use Illuminate\Support\Facades\Log;
 
 class TelegramBotController extends Controller
 {
-     public function webhook(Request $request)
+    public function webhook(Request $request)
     {
-        $update = Telegram::getWebhookUpdates();
+        $update = $request->all(); // Берём весь JSON от Telegram
 
-        $chatId = $update->getMessage()->getChat()->getId();
-        $text = $update->getMessage()->getText();
+        Log::info('Telegram update:', $update); // Логируем для проверки
 
-        // Простейший ответ
-        Telegram::sendMessage([
-            'chat_id' => 6415801830,
-            'text' => "Вы написали: $text"
-        ]);
+        if (isset($update['message']['chat']['id'])) {
+            $chatId = $update['message']['chat']['id'];
+            $text = $update['message']['text'] ?? '';
+
+            // Простейший ответ
+            try {
+                Telegram::sendMessage([
+                    'chat_id' => $chatId,
+                    'text' => "Вы написали: $text"
+                ]);
+            } catch (\Exception $e) {
+                Log::error('Telegram sendMessage error: ' . $e->getMessage());
+            }
+        }
 
         return response()->json(['status' => 'ok']);
-
-        
-}
-
+    }
 }
