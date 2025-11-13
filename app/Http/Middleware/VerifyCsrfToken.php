73d94943486bb29e71ca90a4eb165d5a21de7ad4
Vasilii Sukhorukov <vasilii.matrix@gmail.com>
2025-11-14 00:59:39 +0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/telegram/webhook',
+        'telegram/webhook',
+        '/telegram/webhook/'
     ];
 }
